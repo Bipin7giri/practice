2a645bce68e4c06cbcd5bce9d07ba297cc5c6b42
Bipin Giri <bipingiri27@gmail.com>
2022-04-24 14:51:09 +0545

    adding column

--- database/migrations/2022_04_24_090432_add_address_to_first.php
+++ database/migrations/2022_04_24_090432_add_address_to_first.php
@@ -13,7 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('first', function (Blueprint $table) {
+            $table->string("address");
+        });
     }
 
     /**
@@ -23,6 +25,8 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('first', function (Blueprint $table) {
+            //
+        });
     }
 };
